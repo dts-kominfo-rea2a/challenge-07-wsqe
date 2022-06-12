83ca4a0e4f2cc6077cf0f1fbe76cf4fcf2c64375
winner <winnerson.purba@mail.ugm.ac.id>
2022-06-12 20:49:01 +0700

    try challenge

--- promise.js
+++ promise.js
@@ -1,7 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (keyword) => {
+  try {
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+
+    const data = await [...theaterIXX, ...theaterVGC];
+
+    const countValue = data.filter(item => {
+      return item.hasil == keyword
+    }).length;
+
+    return countValue
+  } catch (err) {
+    console.error(err)
+  }
+};
 
 module.exports = {
   promiseOutput,
